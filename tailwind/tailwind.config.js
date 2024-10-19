3d819237126acbd65c44bc8a47c06953d889436d
Adityasinghvats <131326798+Adityasinghvats@users.noreply.github.com>
2024-10-19 17:10:46 +0530

    tailwind css with local setup done

---
+++ tailwind/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // This means inside main directory there is a src folder with the files of extension
+  // .html and .js it will target them directly
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
